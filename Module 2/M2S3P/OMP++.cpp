5583a52604ccd7645a6efc1b58e98afcf5af654b
adityaadua <133054443+adityaadua@users.noreply.github.com>
2024-02-25 15:57:57 +0530

    Add files via upload

---
+++ Module 2/M2S3P/OMP++.cpp
@@ -0,0 +1,79 @@
+#include <chrono>
+#include <cstdlib>
+#include <iostream>
+#include <omp.h>
+#include <time.h>
+
+using namespace std::chrono;
+using namespace std;
+
+void randomVector(int vector[], int size) {
+  for (int i = 0; i < size; i++) {
+    vector[i] = rand() % 100;
+  }
+}
+
+int main() {
+  unsigned long size = 100000000;
+  srand(time(0));
+  int *v1, *v2, *v3;
+  auto start = high_resolution_clock::now();
+  v1 = (int *)malloc(size * sizeof(int));
+  v2 = (int *)malloc(size * sizeof(int));
+  v3 = (int *)malloc(size * sizeof(int));
+  randomVector(v1, size);
+  randomVector(v2, size);
+
+  // OpenMP parallel region with default(none) attribute
+  #pragma omp parallel default(none) shared(size, v1, v2, v3)
+  {
+    // OpenMP parallel for directive for parallel loop
+    #pragma omp for
+    for (int i = 0; i < size; i++) {
+      v3[i] = v1[i] + v2[i];
+    }
+
+    // Compute total sum using reduction clause
+    int total_reduction = 0;
+    #pragma omp for reduction(+:total_reduction)
+    for (int i = 0; i < size; i++) {
+      total_reduction += v3[i];
+    }
+
+    // Compute total sum using atomic update
+    int total_atomic = 0;
+    #pragma omp for reduction(+:total_atomic)
+    for (int i = 0; i < size; i++) {
+      total_atomic += v3[i];
+    }
+
+    // Compute total sum with critical section
+    int total_critical = 0;
+    #pragma omp for reduction(+:total_critical)
+    for (int i = 0; i < size; i++) {
+      total_critical += v3[i];
+    }
+
+    // OpenMP barrier to synchronize threads
+    #pragma omp barrier
+
+    // Print total sum computed using different methods
+    #pragma omp single
+    {
+      cout << "Total sum (Reduction): " << total_reduction << endl;
+      cout << "Total sum (Atomic): " << total_atomic << endl;
+      cout << "Total sum (Critical): " << total_critical << endl;
+    }
+  }
+
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<microseconds>(stop - start);
+  cout << "Time taken by function :" << duration.count() << " microseconds"
+       << endl;
+
+  free(v1);
+  free(v2);
+  free(v3);
+
+  return 0;
+}

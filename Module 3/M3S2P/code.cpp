aa3352df317f9f36571019c6db0ae9369882b205
adityaadua <133054443+adityaadua@users.noreply.github.com>
2024-03-31 12:58:20 +0530

    code.cpp

--- Module 3/M3S2P/code.cpp
+++ Module 3/M3S2P/code.cpp
@@ -22,3 +22,27 @@ int main(int argc, char *argv[]) {
     MPI_Finalize();
     return 0;
 }
+
+// Part 2
+#include <stdio.h>
+#include <string.h>
+#include <mpi.h>
+
+int main(int argc, char *argv[]) {
+    int rank, size;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (rank == 0) { // Master process
+        char message[] = "Hello World!";
+        MPI_Bcast(message, strlen(message) + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+    } else { // Worker processes
+        char recv_message[100];
+        MPI_Bcast(recv_message, 100, MPI_CHAR, 0, MPI_COMM_WORLD);
+        printf("Rank %d received: %s\n", rank, recv_message);
+    }
+
+    MPI_Finalize();
+    return 0;
+}
